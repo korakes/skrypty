f907c415ddf157b7f9e35c9ee2d7e98f19136704
korakes <32980551+korakes@users.noreply.github.com>
2018-05-17 14:50:10 +0200

    Dodanie wylacz/wlacz wyprawki

--- wyprawki.js
+++ wyprawki.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wyprawki
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @match        https://r*.bloodwars.interia.pl/?a=quest
 // @grant        none
 // @include     https://r*.bloodwars.interia.pl/?a=quest
@@ -9,6 +9,12 @@
 
 function Main() {
     const on = localStorage.getItem('Wyprawki');
+	const menu = document.getElementsByClassName('menu');
+	if(on == 'true') {
+		menu[12].innerHTML += '<li style="padding: 1px; margin-bottom: -5px; font-size: 10px;"><span style="background: transparent; color: #F3F877; text-decoration: none; font-weight: bold; text-align: center; cursor: pointer;" onclick="localStorage.setItem(`Wyprawki`, `false`); location.reload();">OFF</span>';
+	} else {
+		menu[12].innerHTML += '<li style="padding: 1px; margin-bottom: -5px; font-size: 10px;"><span style="background: transparent; color: #F3F877; text-decoration: none; font-weight: bold; text-align: center; cursor: pointer;" onclick="localStorage.setItem(`Wyprawki`, `true`); location.reload();">ON</span>';
+	}
 
     if(on != 'true') {
      console.log('Wyprawki są wyłączone');
@@ -17,7 +23,7 @@ function Main() {
     if(!document.getElementById('quest_timeleft')) {
      document.getElementById('startQuest').click();
     }
-    const timerStop = Math.floor(Math.random() * 8000) + 3000;
+    const timerStop = Math.floor(Math.random() * 3000) + 1000;
     const odliczanie = setInterval(function(){
     const time = document.getElementById('quest_timeleft').innerText;
         if(time.search('Zakończono') == 0) {
